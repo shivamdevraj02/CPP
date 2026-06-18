d47c3ab0801b628d8f6beee502fe06d1a0b22abc
Devraj <devrajshivam02@gmail.com>
2026-06-18 10:15:38 +0530

    time and space complexity

---
+++ time_space_complexity.cpp/basic.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+constant time --->  O(1)
+linear time --> O(n)
+logarithmic time -->O(logn)
+qadratic time --> O(n2)
+cubic time --> O(n3)
+
+note--
+
+Above all these are the standard way of the representing the time in the programming languages ,
+
+NOTE :-
+
+Space cpmplexity :- space complexity can be defined as the total memory taken by the programm .
