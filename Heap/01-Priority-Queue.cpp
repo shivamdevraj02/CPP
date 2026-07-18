83eef37b7b8bcde2586a42070a7f1b291d9dc23f
Devraj <devrajshivam02@gmail.com>
2026-07-18 19:10:30 +0530

    ..

---
+++ Heap/01-Priority-Queue.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    priority_queue<int>p;  // max heap
+
+    p.push(12);
+    p.push(121);
+
+    p.push(13);
+    p.push(120);
+    p.push(122);
+
+    cout<<p.top()<<endl<<" ";
+
+    p.pop();
+
+    cout<<p.top()<<endl<<" ";
+    cout<<p.size()<<endl<<" ";
+
+    while (!p.empty())
+    {
+       cout<<p.top()<<" ";
+       p.pop();
+    }
+    
+
+    
+
+    return 0;
+}
